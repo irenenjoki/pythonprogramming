cc37af9f7c832aa18f3b61fe79d263b03648c4e0
Irene Waweru <85219856+irenenjoki@users.noreply.github.com>
2023-06-05 12:08:23 +0300

    Add files via upload

--- ifstatement.py
+++ ifstatement.py
@@ -1,5 +1,5 @@
 # Prompt the user to input three numbers
-num1 = float(input("Enter the first number: "))
+""""num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 num3 = float(input("Enter the third number: "))
 
@@ -12,4 +12,22 @@ else:
     largest = num3
 
 # Print the largest number
-print("The largest number is:", largest)
+print("The largest number is:", largest)"""
+
+
+    # Prompt the user to input salary and years of service
+salary = float(input("Enter the employee's salary: "))
+years_of_service = int(input("Enter the years of service: "))
+
+# Calculate the bonus amount based on the years of service
+if years_of_service > 10:
+    bonus_percent = 0.10
+elif years_of_service >= 6:
+    bonus_percent = 0.08
+else:
+    bonus_percent = 0.05
+
+bonus_amount = salary * bonus_percent
+
+# Print the bonus amount
+print("Bonus Amount:", bonus_amount)
