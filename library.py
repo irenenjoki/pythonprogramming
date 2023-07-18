50baebf4ed2c5da1c40ecb7123c08a18b267a890
Irene Waweru <85219856+irenenjoki@users.noreply.github.com>
2023-07-18 22:45:46 +0300

    Add files via upload

--- library.py
+++ library.py
@@ -4,29 +4,22 @@ class Book:
         self.title = title
         self.author = author
         self.publication_year = publication_year
-
     def display_info(self):
         print("Title:", self.title)
         print("Author:", self.author)
         print("Publication Year:", self.publication_year)
-
 title=input("input book title\n")
 author=input("input book author\n")
 publication_year=int(input("enter publication year\n"))
-
 book_object = Book(title, author, publication_year)
-
- #create a class called LibraryMember
-
-class LibraryMember(Book):
+#create a class called LibraryMember
+class LibraryMember:
     def __init__(self, member_id, name):
         self.member_id = member_id
         self.name = name
         self.borrowed_books =[] 
-
     def borrow_book(self, book):
         self.borrowed_books.append(book)
-
     def return_book(self, book):
         if book in self.borrowed_books:
             self.borrowed_books.remove(book)
@@ -34,27 +27,21 @@ class LibraryMember(Book):
     def display_info(self):
         print("Member ID:", self.member_id)
         print("Name:", self.name)
-        print("number of borrowed books:")
+        print("number of borrowed books:",len(self.borrowed_books))
 #input member info        
 member_id=input("input member id\n")
 name=input("input member name\n")
-book=int(input("input number of books borrowed\n"))
-
+#create a new instance of the Book class for each book borrowed
+num_book=int(input("input number of books borrowed\n"))
 # Create library member object
 member = LibraryMember(member_id, name)
 # Borrow books
-for _ in range(book):
-    member.borrow_book(book_object)
-
-member.borrow_book(book)
-
-
+for _ in range(num_book):
+    member.borrow_book(Book(title, author, publication_year))
 # Display member information
-member.display_info()
-
+book_object.display_info()
 # Return a book
 member.return_book(book_object)
-for _ in range(book):
-    member.return_book(book_object)
+
 # Display updated member information
-book_object.display_info()
+member.display_info()
